af32476a4bf9b7f93f21ad1052c8c44b26480dd0
Ismail Irshad <irshadsha164@gmail.com>
2026-03-27 10:20:55 +0530

    server.js file updated for vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,52 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/authRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import cartRoutes from './routes/cartRoutes.js';
-import couponRoutes from './routes/couponRoutes.js';
-import paymentRoutes from './routes/paymentRoutes.js';
-import analyticsRoutes from './routes/analyticsRoutes.js'
-import connectDB from './config/db.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import couponRoutes from "./routes/couponRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import analyticsRoutes from "./routes/analyticsRoutes.js";
+import connectDB from "./config/db.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+connectDB();
 
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:5000',
-    "http://luxora.ismailirshad.in",
-  ],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5000",
+      "http://luxora.ismailirshad.in",
+      "https://luxora-hazel.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 
-app.use(express.json({ limit: '10mb' }));
-app.use(cookieParser())
-app.use("/api/auth", authRoutes)
-app.use("/api/products", productRoutes)
-app.use("/api/cart", cartRoutes)
-app.use("/api/coupons", couponRoutes)
-app.use("/api/payments", paymentRoutes)
-app.use("/api/analytics", analyticsRoutes)
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
 
-app.listen(PORT, () => {
-  console.log('server is running on http://localhost:' + PORT)
-  connectDB();
-})
+app.get("/", (req, res) => {
+  res.send("Luxora backend is running");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
+// Local development only
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
+  });
+}
+
+export default app;
